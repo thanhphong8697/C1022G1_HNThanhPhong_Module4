b318d6f0e3b129e5bd32b92fbb5de32de53ab153
thanhphong8697 <Thanhphong861997@gmail.com>
2023-03-09 19:34:24 +0700

    up

--- ss9_Exception/book_app/src/main/java/com/example/book_app/controller/BookController.java
+++ ss9_Exception/book_app/src/main/java/com/example/book_app/controller/BookController.java
@@ -1,7 +1,21 @@
 package com.example.book_app.controller;
 
+import com.example.book_app.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/book")
 public class BookController {
+    @Autowired
+    public IBookService iBookService;
+
+    @GetMapping("")
+    public String showList(Model model){
+        model.addAttribute("bookDTO", iBookService.listAll());
+        return "/List";
+    }
 }
--- ss9_Exception/book_app/src/main/java/com/example/book_app/modelDTO/BookDTO.java
+++ ss9_Exception/book_app/src/main/java/com/example/book_app/modelDTO/BookDTO.java
@@ -1,4 +1,28 @@
 package com.example.book_app.modelDTO;
 
-public class BookDTO {
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.NotBlank;
+
+public class BookDTO implements Validator {
+    private Integer id;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private Integer quantity;
+    @NotBlank
+    private Integer price;
+    @NotBlank
+    private String code;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+    }
 }
